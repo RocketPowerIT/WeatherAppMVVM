dd825394766473991196ce0e281449b4f943712d
Dmitry Kokurkin <wolfyteze@gmail.com>
2021-03-17 16:18:41 +0300

    Initial Commit

---
+++ WeatherAppMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppMVVM
+//
+//  Created by wolfyteze on 17/03/2021.
+//  Copyright © 2021 wolfyteze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
